04785227872d5401c58a4a012ce3d7db20ff4f42
ivaylo-milanov <imilanov24@gmail.com>
2025-03-31 14:04:23 +0300

    change filter grid

--- client/src/components/Catalog/catalog-filter/CatalogFilter.jsx
+++ client/src/components/Catalog/catalog-filter/CatalogFilter.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import ClubField from "../../inputs/club-field/ClubField";
 
-const CatalogFilter = ({ searchParams, changeSearchParams }) => { 
+const CatalogFilter = ({ searchParams, changeSearchParams }) => {
     const [club, setClub] = useState("");
 
     const filterHandler = (formData) => {
@@ -16,7 +16,7 @@ const CatalogFilter = ({ searchParams, changeSearchParams }) => {
     const sortHandler = (e) => {
         const value = e.target.value;
 
-        const [field, dir] = value.split('_');
+        const [field, dir] = value.split("_");
 
         changeSearchParams({
             sort: { dir: dir, field: field },
@@ -24,16 +24,22 @@ const CatalogFilter = ({ searchParams, changeSearchParams }) => {
     };
 
     const returnSortValue = () => {
-        return `${searchParams.get("sortField")}_${searchParams.get("sortDir")}`;
-    }
+        return `${searchParams.get("sortField")}_${searchParams.get(
+            "sortDir"
+        )}`;
+    };
 
     return (
         <div className="w-full bg-white p-8 rounded-2xl shadow-xl mt-10">
-            <h2 className="text-3xl font-medium text-gray-900 mb-8 text-center">Event Filters</h2>
+            <h2 className="text-3xl font-medium text-gray-900 mb-8 text-center">
+                Event Filters
+            </h2>
             <form action={filterHandler} className="space-y-6">
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                <div className="grid grid-cols-3 sm:grid-cols-3 gap-6">
                     <div className="flex flex-col">
-                        <label className="text-sm font-semibold text-gray-700">Club</label>
+                        <label className="text-sm font-semibold text-gray-700">
+                            Club
+                        </label>
                         <ClubField
                             onChange={changeClubHandler}
                             club={club}
@@ -42,7 +48,9 @@ const CatalogFilter = ({ searchParams, changeSearchParams }) => {
                     </div>
 
                     <div className="flex flex-col">
-                        <label className="text-sm font-semibold text-gray-700">Event Year</label>
+                        <label className="text-sm font-semibold text-gray-700">
+                            Event Year
+                        </label>
                         <input
                             defaultValue={searchParams.get("eventDate") || ""}
                             type="text"
@@ -51,28 +59,44 @@ const CatalogFilter = ({ searchParams, changeSearchParams }) => {
                             className="mt-2 p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-150 ease-in-out"
                         />
                     </div>
-                </div>
 
-                <div className="flex flex-col">
-                    <label className="text-sm font-semibold text-gray-700">Search Events</label>
-                    <input
-                        defaultValue={searchParams.get("eventName") || ""}
-                        name="eventName"
-                        type="text"
-                        placeholder="Search events"
-                        className="mt-2 p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-150 ease-in-out"
-                    />
+                    <div className="flex flex-col">
+                        <label className="text-sm font-semibold text-gray-700">
+                            Search Events
+                        </label>
+                        <input
+                            defaultValue={searchParams.get("eventName") || ""}
+                            name="eventName"
+                            type="text"
+                            placeholder="Search events"
+                            className="mt-2 p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-150 ease-in-out"
+                        />
+                    </div>
                 </div>
 
                 <div className="flex justify-between items-center">
                     <div className="flex items-center space-x-4">
-                        <label className="text-sm font-medium text-gray-600">Sort</label>
-                        <select onChange={sortHandler} value={returnSortValue() || ""} className="px-4 py-2 bg-white border border-gray-300 rounded-md text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 hover:border-indigo-500 transition duration-200 ease-in-out">
+                        <label className="text-sm font-medium text-gray-600">
+                            Sort
+                        </label>
+                        <select
+                            onChange={sortHandler}
+                            value={returnSortValue() || ""}
+                            className="px-4 py-2 bg-white border border-gray-300 rounded-md text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 hover:border-indigo-500 transition duration-200 ease-in-out"
+                        >
                             <option value="">Select Sort</option>
-                            <option value="eventDate_asc">Event Date - Ascending</option>
-                            <option value="eventDate_desc">Event Date - Descending</option>
-                            <option value="eventName_asc">Event Name - Ascending</option>
-                            <option value="eventName_desc">Event Name - Descending</option>
+                            <option value="eventDate_asc">
+                                Event Date - Ascending
+                            </option>
+                            <option value="eventDate_desc">
+                                Event Date - Descending
+                            </option>
+                            <option value="eventName_asc">
+                                Event Name - Ascending
+                            </option>
+                            <option value="eventName_desc">
+                                Event Name - Descending
+                            </option>
                         </select>
                     </div>
 
